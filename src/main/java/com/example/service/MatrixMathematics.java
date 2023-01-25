c936974db52ea7cd4ea42ed640496f55e17cc2be
Ludmila <jl_amriou@esi.dz>
2023-01-25 09:44:33 +0100

    25th commit

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.exception.NoSquareException;
 import com.example.model.Matrix;
-import org.graalvm.compiler.hotspot.stubs.DivisionByZeroExceptionStub;
 
 public class MatrixMathematics {
 
@@ -116,22 +115,12 @@ public class MatrixMathematics {
 	 * @throws NoSquareException
 	 */
 	public static Matrix inverse(Matrix matrix) throws NoSquareException {
-		if (determinant(matrix) == 0){
-			return null;
-		}
-		else {
-			return (transpose(cofactor(matrix)).multiplyByConstant(1.0 / determinant(matrix)));
-		}
+		double d = determinant(matrix);
+
+		if (d != 0){
+		return (transpose(cofactor(matrix)).multiplyByConstant(1.0/d));
+	}
+		return null;
 	}
 
-
-	public  void test() {
-
-	}
-
-
-
-
-	
-	
 }
